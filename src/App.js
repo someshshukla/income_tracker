9466d69604701a2a5cd74e01d39467731279c9f6
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-16 13:01:10 +0530

    deploying

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useState, useEffect} from "react";
+import Header from './components/Header';
+import IncomeForm from "./components/IncomeForm";
+import IncomeList from './components/IncomeList';
 function App() {
+
+  const [income, setIncome] = useState([]);
+  const [totalIncome, setTotalIncome] = useState(0);
+
+
+  useEffect(() => {
+    let temp = 0;
+    for(let i = 0; i < income.length; i++){
+      temp += parseInt(income[i].price);
+    }
+    setTotalIncome(temp);
+  }, [income]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        Income Tracker
-      </header>
+      <Header totalIncome={totalIncome} />
+      <IncomeForm income={income} setIncome={setIncome}/>
+      <IncomeList income={income} setIncome={setIncome}/>
     </div>
   );
 }
